5848cc5ff8968d65c087a278ae75f436ae98577e
YAP! <GMADDA200@caledonian.ac.uk>
2017-06-02 15:35:38 +0100

    hooked animation to script

---
+++ Assets/Game/Code/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    #region member variables
+
+    public GameObject m_shard;
+    public Animator m_anim;
+
+    #endregion
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if (Input.GetMouseButtonDown(0))
+        {
+            m_anim.SetBool("shooting", true);
+            m_shard.SetActive(false);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            m_anim.SetBool("shooting", false);
+            m_shard.SetActive(true);
+        }
+	}
+}
